1bd4ed081fba037697b365e54ebca55bf508f3a2
GustavoPrescinotti <gustavo.prescinottisenai@gmail.com>
2026-04-14 08:57:41 -0300

    Realizacao do cadastro de marca
    Foi adicionado 3 rotas de marca a de edicao a de cadastro e a de deletar a marca

--- carro.py
+++ carro.py
@@ -166,32 +166,71 @@ def editar_carro(id_veiculo):
     finally:
         cur.close()
 
-@app.route('/excluir_carro/<id_carro>', methods=['DELETE'])
-def excluir_carro(id_carro):
 
-
-@app.route('/registrar_venda',methods=['POST'])
-def registrar_venda():
+@app.route('/cadastrar_marca', methods=['POST'])
+def cadastrar_marca():
     cur = con.cursor()
-    token = request.form.get('token')
-    if not token:
-        return jsonify({'erro': 'Acesso negado. Token não encontrado.'}), 401
+
+
+
     try:
-        payload = jwt.decode(token, app.config['SECRET_KEY'])
-        id_adm = payload['id_user']
-        cur.execute("SELECT TIPO_USUARIO FROM USUARIO WHERE ID_USUARIO = ?", (id_adm,))
-        usuarios = cur.fetchone()
-        if not usuarios or usuarios[0] != 2:
-            return jsonify({'erro': 'Acesso restrito. Apenas Administradores podem acessar.'}), 403
 
-        id_venda = request.form.get('id_venda')
-        id_usuario = request.form.get('id_usuario')
-        id_veiculo = request.form.get('id_veiculo')
-        forma_pagamento = request.form.get('formato_pagamento')
-        data_venda = request.form.get('data_venda')
-        valor_venda = request.form.get('valor_venda')
-        valor_recebido = request.form.get('valor_recebido')
-        status_pagamento = request.form.get('status_pagamento')
-        comentarios = request.form.get('comentarios')
-        descontos = request.form.get('descontos')
 
+
+        marca = request.form.get('marca')
+        if not marca or marca.strip() == "":
+            return jsonify({'erro': 'O nome da marca é obrigatório.'}), 400
+
+        nome_marca = marca.strip().upper()
+
+
+        cur.execute("SELECT ID_MARCA FROM MARCA WHERE LOWER(MARCA) = LOWER(?)", (nome_marca,))
+        if cur.fetchone():
+            return jsonify({'erro': 'Essa marca já foi cadastrada.'}), 409
+
+
+        cur.execute("INSERT INTO MARCA (MARCA) VALUES (?)", (nome_marca,))
+        con.commit()
+
+        return jsonify({'mensagem': 'Marca cadastrada com sucesso!'}), 201
+
+    except jwt.ExpiredSignatureError:
+        return jsonify({"erro": "Sessão expirada."}), 401
+    except Exception as e:
+        con.rollback()
+        return jsonify({'erro': f'Erro ao cadastrar: {e}'}), 500
+    finally:
+        cur.close()
+
+@app.route('/editar_marca/<int:id_marca>',methods=['PUT'])
+def editar_marca(id_marca):
+    cur = con.cursor()
+    try:
+        nova_marca = request.form.get('nova_marca')
+        if not nova_marca:
+            return jsonify({'erro':'o nome da marca e obrigatorio'}),400
+        cur.execute("UPDATE MARCA SET MARCA = ? WHERE ID_MARCA = ?",(nova_marca.strip().upper(),id_marca))
+        con.commit()
+
+        return jsonify({'mensagem': 'Marca editada com sucesso!'}), 200
+    except Exception as e:
+        return jsonify({'erro':f'Erro ao editar marca: {e}'}), 500
+    finally:
+        cur.close()
+
+@app.route('/deletar_marca/<int:id_marca>',methods=['DELETE'])
+def deletar_marca(id_marca):
+    cur = con.cursor()
+    try:
+      cur.execute('DELETE FROM MARCA WHERE ID_MARCA = ?', (id_marca,))
+      con.commit()
+      return jsonify({'mensagem':'Marca deletada com sucesso!'}), 200
+    except Exception as e:
+        return jsonify({'erro':f'Erro ao deletar marca: {e}'}), 500
+    finally:
+        cur.close()
+
+
+
+
+#
