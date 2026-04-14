ab70f1a5a77ec7feb33a3cbb115d48f91a700b2d
GustavoPrescinotti <gustavo.prescinottisenai@gmail.com>
2026-04-14 14:05:27 -0300

    foi adicionado e foi atualizado novas rotas
    Foi adicionado novas rotas cadastrar_servico,listar_carro,excluir_carro

--- carro.py
+++ carro.py
@@ -165,6 +165,49 @@ def editar_carro(id_veiculo):
         return jsonify({'erro': f'Erro ao editar carro: {e}'}), 500
     finally:
         cur.close()
+@app.route('/excluir_carro/<id_veiculo>', methods=['DELETE'])
+def excluir_carro(id_veiculo):
+    cur = con.cursor()
+    try:
+        cur.execute("""DELETE FROM VEICULO WHERE ID_VEICULO = ?""", (id_veiculo,))
+        con.commit()
+
+        nome_imagem = f'veico_{id_veiculo}.png'
+        caminho_foto = os.path.join(app.config['UPLOAD_FOLDER'], nome_imagem)
+        if os.path.isfile(caminho_foto):
+            os.remove(caminho_foto)
+            return jsonify({'mensagem': 'Carro excluido com sucesso!'}), 200
+    except Exception as e:
+        con.rollback()
+        return jsonify({'erro': f'Erro ao excluir carro: {e}'}), 500
+    finally:
+        cur.close()
+
+@app.route('/listar_carro', methods=['GET'])
+def listar_carro():
+    cur = con.cursor()
+    try:
+        cur.execute(
+            "SELECT V.ID_VEICULO, M.MARCA, V.MODELO, V.ANO_FABRICACAO, V.ANO_MODELO, V.PRECO, V.PLACA, V.COR FROM VEICULO V INNER JOIN MARCA M ON V.ID_MARCA = M.ID_MARCA")
+        carros = cur.fetchall()
+
+        lista_carros = []
+        for c in carros:
+            lista_carros.append({
+                'ID_VEICULO': c[0],
+                'MARCA': c[1],
+                'MODELO': c[2],
+                'ANO_FABRICACAO': c[3],
+                'ANO_MODELO': c[4],
+                'PRECO': c[5],
+                'PLACA': c[6],
+                'COR': c[7],
+            })
+            return jsonify({'carros': lista_carros}),200
+    except Exception as e:
+        return jsonify({'erro':f'Erro ao ler carro: {e}'}), 500
+    finally:
+        cur.close()
 
 
 @app.route('/cadastrar_marca', methods=['POST'])
@@ -254,11 +297,46 @@ def deletar_marca(id_marca):
 
         if not usuarios or usuarios[0] != 2:
             return jsonify({'erro': 'Acesso restrito. Apenas Administradores pode acessar'}), 403
-        
+
         cur.execute('DELETE FROM MARCA WHERE ID_MARCA = ?', (id_marca,))
         con.commit()
         return jsonify({'mensagem':'Marca deletada com sucesso!'}), 200
     except Exception as e:
         return jsonify({'erro':f'Erro ao deletar marca: {e}'}), 500
+    finally:
+        cur.close()
+
+
+
+@app.route('/cadastrar_servico', methods=['POST'])
+def cadastrar_servico():
+    cur = con.cursor()
+    try:
+
+        nome_servico = request.form.get('nome_servico')
+        valor = request.form.get('valor')
+
+        if not nome_servico or not valor:
+            return jsonify({'erro':'O nome do serviço e o valor são obrigatórios.'}),400
+
+        nome_servico = nome_servico.strip().upper()
+
+        try:
+            valor = float(valor.replace(',','.'))
+            if valor <= 0:
+                return jsonify({'erro':'O valor serviço deve ser maior que zero'}),400
+        except ValueError:
+            return jsonify({'erro':'Valor invalido.Digite apenas número '}),400
+        cur.execute("""SELECT ID_SERVICO FROM SERVICO WHERE LOWER(NOME_SERVICO)= LOWER(?)""", (nome_servico,))
+        if cur.fetchone():
+            return jsonify({'erro':'Serviço já cadastrado'})
+
+        cur.execute("INSERT INTO SERVICO (NOME_SERVICO, VALOR) VALUES (?, ?)", (nome_servico, valor))
+        con.commit()
+
+        return jsonify({'menssagem':'Seviço cadastrado com sucesso!'}), 201
+
+    except Exception as e:
+        return jsonify({'erro':f'Erro ao cadastrar: {e}'}), 500
     finally:
         cur.close()
